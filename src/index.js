bf0412281c036c5400620cf42c1695caccf8581b
shadypollen <coplayr@hotmail.com>
2018-07-15 20:54:32 +0100

    Added hooks to efficiently update components

--- src/index.js
+++ src/index.js
@@ -1,44 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Comment(props) {
-	console.log(props);
-	return(
-		<div className="Comment">
-			<div className="UserInfo">
-				<Avatar author={props.author} />
-				<UserName author={props.author} />
+class Clock extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {date: new Date()};
+	}
+	render() {
+		return (
+			<h1>It is {this.state.date.toLocaleTimeString()}</h1>
+		);
+	}
+	componentDidMount() {
+		this.timerID = setInterval( () => this.tick(), 1000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+	tick() {
+		this.setState({
+			date: new Date(),
+		});
+	}
+};
+
+class ClockWall extends React.Component {
+	render() {
+		return (
+			<div>
+				<Clock />
+				<Clock />
+				<Clock />
 			</div>
-			<CommentText text = {props.text} />
-			<CommentDate date = {props.date} />
-		</div>
-	);
-}
-
-function Avatar(props) {
-	return <a className="Avatar" href={props.author.avatarUrl}>
-			{props.author.avatarUrl}
-			</a>
+		);
+	};
 };
 
-function UserName(props) {
-	return <div className="UserInfo-name"> {props.author.name} </div>
-};
-
-function CommentText(props) {
-	return <div className="Comment-text"> {props.text} </div>
-};
-
-function CommentDate(props) {
-	return <div className="Comment-date"> {props.date} </div>
-};
-
-let author = {name : "Aleks",
-			  avatarUrl : "lalkapalka.com"
-			 };
-
-let text = "lolololol";
-let date = new Date().toLocaleTimeString();	
-
-ReactDOM.render(<Comment author={author} text={text} date={date} />, document.getElementById("root"));
-
+ReactDOM.render(<ClockWall />, document.getElementById("root"));
